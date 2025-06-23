7e9406e00d2573e4556e4ef0c9c69080bfb5c26e
blackbox24 <nicardid@gmail.com>
2025-06-23 03:20:40 +0000

    feature: added .env variable

--- database/db.js
+++ database/db.js
@@ -1,11 +1,12 @@
 // db.js
 const mysql = require("mysql2/promise");  // Use mysql2/promise
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost", // Replace with your DB host
-  user: 'root',
-  password: 'Xs_shellsz202322',
-  database: "homeStyle", // Replace with your DB name
+  host: process.env.DB_HOST, // Replace with your DB host
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, // Replace with your DB name
   waitForConnections: true,
   connectionLimit: 20, // Adjust based on your needs
   queueLimit: 0
